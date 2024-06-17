c27d8b4aee9ef1b2ae4bc89f28bbcdf17da32c81
Aftab Nabi <aftab.nabi21@gmail.com>
2024-06-17 18:44:03 +0500

    Tweak the text

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 #! /usr/bin/env node
+// npm login in cmd
+// npm publish in cmd
+// npx (name of package) published on npm
 import inquirer from "inquirer";
 // npm install inquirer
 // npm install --save-dev @types/inquirer
 const answer = await inquirer.prompt([
-    { message: "Enter First Number", type: "number", name: "firstNumber" },
-    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    { message: "Enter Your First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Your Second Number", type: "number", name: "secondNumber" },
     {
         message: "List one of the operator to perform operation",
         type: "list",
@@ -12,7 +15,7 @@ const answer = await inquirer.prompt([
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
     },
 ]);
-console.log(answer);
+// console.log(answer);
 // conditional statement
 if (answer.operator === "Addition") {
     console.log(answer.firstNumber + answer.secondNumber);
